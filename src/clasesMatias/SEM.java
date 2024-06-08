8659ebbd13f2c91cee836b82d7f7b35a704d32cc
Matias <Matias@DESKTOP-T2M1K30>
2024-06-08 18:16:59 -0300

    Creacion de algunas clases y avance en Test de la clase SEM

--- src/clasesMatias/SEM.java
+++ src/clasesMatias/SEM.java
@@ -1,5 +1,21 @@
 package clasesMatias;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SEM {
+
+	private List<ZonaDeEstacionamiento> zonasEstacionamiento = new ArrayList<ZonaDeEstacionamiento>();
+
+	public void añadirZonaDeEstacionamiento(ZonaDeEstacionamiento zona) {
+		this.getZonasDeEstacionamiento().add(zona);
+		
+	}
+	
+
+	public List<ZonaDeEstacionamiento> getZonasDeEstacionamiento() {
+		
+		return this.zonasEstacionamiento;
+	}
 
 }
