e2cd5b98290c6849d20f2a26f70adf37cdad0bad
Matias <Matias@DESKTOP-T2M1K30>
2024-06-08 18:40:23 -0300

    avance en testeo de ZonaDeEstacionamiento

--- src/clasesMatias/ZonaDeEstacionamiento.java
+++ src/clasesMatias/ZonaDeEstacionamiento.java
@@ -1,5 +1,32 @@
 package clasesMatias;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mockito.ArgumentMatchers;
+
 public class ZonaDeEstacionamiento {
+
+	private Inspector inspectorAsignado;
+	private List<PuntoDeVenta> puntosDeVenta = new ArrayList<PuntoDeVenta>();;
+
+	public ZonaDeEstacionamiento(Inspector inspector) {
+		this.inspectorAsignado = inspector;
+	}
+
+	public void añadirPuntoDeVenta(PuntoDeVenta puntoVenta) {
+		this.getPuntosDeVenta().add(puntoVenta);
+		
+	}
+
+	public List<PuntoDeVenta> getPuntosDeVenta() {
+		
+		return this.puntosDeVenta;
+	}
+
+	public Inspector getInspectorAsignado() {
+		
+		return this.inspectorAsignado;
+	}
 
 }
--- test/clasesMatias/SEMTestCase.java
+++ test/clasesMatias/SEMTestCase.java
@@ -1,12 +1,12 @@
 package clasesMatias;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Test;
 
 
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
 import static org.mockito.Mockito.*;
 
 
