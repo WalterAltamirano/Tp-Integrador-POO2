2c12460a7423f37a49effc0bc8db356f39eb7dce
Matias <Matias@DESKTOP-T2M1K30>
2024-06-08 16:54:10 -0300

    crecion SEMTestCase (prueba)

---
+++ test/testMatias/SEMTestCse.java
@@ -0,0 +1,14 @@
+package testMatias;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SEMTestCse {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
