b66be9f54277ddee1846fdd81b1bb32a6e480850
Walter Altamirano <altamiranow4@gmail.com>
2024-06-12 00:42:22 -0300

    Se implemento en su mayoria la App SEM y el strategy Modo

--- src/clasesWalle/Modo.java
+++ src/clasesWalle/Modo.java
@@ -2,8 +2,29 @@ package clasesWalle;
 
 public abstract class Modo {
 	
-	abstract void inicioDeEstacionamiento(int numeroDeCelular, String patente);
+	public abstract void inicioDeEstacionamiento(AplicacionSEM app,int numeroDeCelular, String patente);
 	
-	abstract void finDeEstacionamiento(int numeroDeCelular);
+	public abstract void finDeEstacionamiento(AplicacionSEM app,int numeroDeCelular);
+	
+	public abstract void notificarAlertaDeInicioDeEstacionamiento(AplicacionSEM app);
+	
+	public abstract void notificarAlertaDeFinDeEstacionamiento(AplicacionSEM app);
+	
+	public abstract boolean estaEnModoAutomatico();
+	
+	protected void puedeEstacionar(AplicacionSEM app,String patente) throws Exception {
+		if(!app.tieneCreditoSuficiente() || app.estaEnZonaDeEstacionamiento()
+				|| app.hayEstacionamientoCon(patente)) {
+			
+			throw new Exception("No se cumplen alguna/as de las condiciones para iniciar un estacionamiento"
+					+ "Saldo: " + app.getCredito() + "Esta una zona valida: " 
+					+ app.estaEnZonaDeEstacionamiento() + "El auto ya esta estacionado: " 
+					+ app.hayEstacionamientoCon(patente));
+		}
+	}
+	
+	protected abstract void avisoDeCambio();
+	
+	protected abstract void darRespuesta(AplicacionSEM app);
 	
 }
--- test/testWalle/AplicacionSEMTest.java
+++ test/testWalle/AplicacionSEMTest.java
@@ -2,6 +2,15 @@ package testWalle;
 
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+
+import clasesIan.Auto;
+import clasesIan.Estacionamiento;
+import clasesIan.EstadoApp;
+import clasesIan.Usuario;
+import clasesMatias.SEM;
+import clasesWalle.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class AplicacionSEMTest {
@@ -16,13 +25,52 @@ public class AplicacionSEMTest {
 	@Test
 	public void testUnUsuarioDecideIniciarUnEstacionamientoConGPSActivadoYModoAutomaticoYLaApliacionSEMIniciaElEstacionamiento() {
 		
+		//SetUp
+		int horaActual = 10;
+		int nroDeCelular = 1123233232;
+		Auto automovil = mock(Auto.class);
+		Modo modoManual = mock(ModoManual.class); 
+		Modo modoAutomatico= mock(Modo.class); 
+		EstadoApp caminando = mock(EstadoApp.class);
+		SEM sistemaEstacionamiento = mock(SEM.class);
+		EstadoGPS gpsEncendido = mock(Encendido.class);
+		Estacionamiento estacionamiento = mock(Estacionamiento.class);
+		Usuario usuario = mock(Usuario.class);
+		
+		
+		AplicacionSEM app = new AplicacionSEM(modoManual,caminando,sistemaEstacionamiento,usuario);
+		
+		when(usuario.patenteDelAuto()).thenReturn("333ALO");
+		when(gpsEncendido.estaEncendido()).thenReturn(true);
+		when(sistemaEstacionamiento.verificarEstacionamientoVigente("333ALO")).thenReturn(false);
+		when(automovil.getPatente()).thenReturn("333ALO");
+		//Excercise
+		app.establecerNroDeCelular(nroDeCelular); //El usuario le provee el nro de celular
+		app.elegirEstadoGPS(gpsEncendido);
+		app.elegirModo(modoAutomatico);
+		app.cargarSaldo(100);
+		app.driving();
+		app.walking(); //Se activaria solo el incio de estacionamiento
+		//app.inicioEstacionamiento(1123233232, "333ALO");
+		
+		assertEquals(100,app.consultarSaldo());
+		
+		//Verify
+		
+		verify(sistemaEstacionamiento).registrarEstacionamiento(estacionamiento);
+		verify(modoAutomatico).inicioDeEstacionamiento(app,nroDeCelular,"333ALO");
+		
+		//Tear Down
+	
+		
 	}
 	@Test
 	public void testUnUsuarioDecideIniciarUnEstacionamientoConGPSDesactivadoYModoAutomaticoYLaApliacionSEMIniciaElEstacionamiento() {
 		
 	}
 	@Test
-	public void test3() {
+	public void testUnSensorIndicaQueElUsuarioPasoDeManejarACaminarEnUnaZonaEstacionamientoYLaAplicacionSEMAlertaAlUsuario() {
+		
 		
 	}
 	@Test
