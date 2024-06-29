1419f449cedbb7e722fdcd8376b801431fdcbbb0
IanB007 <Ian_b@192.168.0.70>
2024-06-29 20:11:00 -0300

    Modificacion de

--- src/clasesIan/Caminando.java
+++ src/clasesIan/Caminando.java
@@ -1,20 +1,44 @@
 package clasesIan;
 
 import clasesWalle.AplicacionSEM;
+import clasesWalle.ModoAutomatico;
 
 public class Caminando extends EstadoApp{
-	
 	@Override
 	public void manejando(AplicacionSEM aplicacion) {
 		System.out.println("El estado actual cambiara a manejando");
 		aplicacion.setEstado(new Caminando());
-		if(aplicacion.getModo().estaEnModoAutomatico()) {
-			aplicacion.finalizarEstacionamiento(aplicacion.getNumeroDeCelular());
-		}
-		aplicacion.alertaFinDeEstacionamiento();
+		//MODIFICACION:
+		aplicacion.pasoAAuto();
 	}
 	@Override
 	public void caminando(AplicacionSEM aplicacion) {
 		System.out.println("El estado actual es caminando");
 	}
 }
+
+/* 
+ * EN MODO GENERAL SERIAN ABSTRACTOS.
+ * IRIA EN CADA MODO:
+  EN MODO AUTOMATICO:
+	  public void finDeEstacionamiento(){
+	  	aplicacion.finalizarEstacionamiento(aplicacion.getNumeroDeCelular());
+	  }
+	  public void inicioEstacionamiento(){
+	  	aplicacion.iniciarEstacionamiento(aplicacion.getNumeroDeCelular(), aplicacion.getUsuario().getPatente())
+	  }
+  
+  ---------------------------------------
+  
+  EN MODO MANUAL:
+	  public void finDeEstacionamiento(){
+	  	System.out.println("Deberias finalizar el estacionamiento!");
+	  }
+	  
+	  public void inicioEstacionamiento(){
+	  	System.out.println("Deberias iniciar el estacionamiento!");
+	  }
+*/
+
+
+
--- src/clasesIan/EnAuto.java
+++ src/clasesIan/EnAuto.java
@@ -3,7 +3,6 @@ package clasesIan;
 import clasesWalle.AplicacionSEM;
 
 public class EnAuto extends EstadoApp {
-
 	@Override
 	public void manejando(AplicacionSEM aplicacion) {
 		System.out.println("El estado actual es en auto");
@@ -12,9 +11,6 @@ public class EnAuto extends EstadoApp {
 	public void caminando(AplicacionSEM aplicacion) {
 		System.out.println("El estado actual cambiara a caminando");	
 		aplicacion.setEstado(new Caminando());
-		if(aplicacion.getModo().estaEnModoAutomatico()) {
-			aplicacion.inicioEstacionamiento(aplicacion.getNumeroDeCelular(), aplicacion.getUsuario().getPatente());
-		}
-		aplicacion.alertaInicioDeEstacionamiento();
+		aplicacion.pasoACaminando();
 	}
 }
