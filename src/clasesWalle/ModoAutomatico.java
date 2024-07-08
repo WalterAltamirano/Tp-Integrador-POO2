8514bc842dfaa3bb87f42cc9430f20051a6ad896
Walter Altamirano <altamiranow4@gmail.com>
2024-07-07 21:11:01 -0300

    Cambios en modo, app y test

--- src/clasesWalle/ModoAutomatico.java
+++ src/clasesWalle/ModoAutomatico.java
@@ -4,32 +4,60 @@ package clasesWalle;
 public class ModoAutomatico implements Modo{
 
 
-	
-	
-	
-//	@Override
-//	public void puedeEstacionar(AplicacionSEM app, String patente) throws ExcepcionPersonalizada{
-//		super.puedeEstacionar(app, patente); 
-//		if(!app.elGpsEstaEncendido()) {
-//			throw new ExcepcionPersonalizada("El gps esta apagado. Para usar el modo automatico tenes que prenderlo");
-//		}
-//	}
-//	
-
 	@Override
 	public void avisoDeCambio() {
 		System.out.print("Usted a elegido el Modo Automatico");
 	}
 
 	@Override
-	public void inicioDeEstacionamiento(AplicacionSEM app) {//Faltan parametros
-		System.out.print("Alerta: Usted esta en modo automatico, por lo que podria iniciarse su estacionamiento");
+	public void inicioDeEstacionamiento(AplicacionSEM app) {
+		try {	
+			this.puedeAlertarInicio(app);
+			this.alertarInicioDeEstacionamiento();
+		}
+		catch(ExcepcionPersonalizada e) {
+		}
 		app.iniciarEstacionamiento();
 	}
 
 	@Override
 	public void finDeEstacionamiento(AplicacionSEM app) {
-		System.out.print("Alerta: Usted esta en modo automatico, por lo que podria finalizarse su estacionamiento");
+		try {
+			this.puedeAlertarFin(app);
+			this.alertarfinDeEstacionamiento();
+		}
+		catch(ExcepcionPersonalizada e) {
+		}
 		app.finalizarEstacionamiento();
+	}
+	
+	//Se envia al usuario si se cumplen las condiciones
+	public void alertarInicioDeEstacionamiento() {
+		System.out.print("Alerta: Usted esta en modo automatico, por lo que podria iniciarse su estacionamiento");
+	}
+	//Se envia al usuario si se cumplen las condiciones
+	public void alertarfinDeEstacionamiento() {
+		System.out.print("Alerta: Usted esta en modo automatico, por lo que podria finalizarse su estacionamiento");
+	}
+	
+	@Override
+	public void puedeAlertarInicio(AplicacionSEM app) throws ExcepcionPersonalizada {
+		this.laAppEstaEnZonaValida(app);
+		if(app.hayEstacionamientoCon(app.getPatente())) {
+			throw new ExcepcionPersonalizada("Ya hay un estacionamiento vigente con la patente dada");
+		}
+	}
+	@Override
+	public void puedeAlertarFin(AplicacionSEM app) throws ExcepcionPersonalizada {
+		this.laAppEstaEnZonaValida(app);
+		if(!app.hayEstacionamientoCon(app.getNumeroDeCelular())) {
+			throw new ExcepcionPersonalizada("No se puede finalizar. No hay estacionamiento vigente.");
+		}
+	}
+	
+	public void laAppEstaEnZonaValida(AplicacionSEM app) throws ExcepcionPersonalizada{
+		if(!app.estaEnZonaDeEstacionamiento()) {
+			throw new ExcepcionPersonalizada("El usuario no esta en una zona de estacionamiento");
+		}
 	}
 }
--- src/clasesWalle/ModoManual.java
+++ src/clasesWalle/ModoManual.java
@@ -3,53 +3,28 @@ package clasesWalle;
 
 public class ModoManual implements Modo{
 
-
-//	@Override
-//	public void darRespuestaInicial(AplicacionSEM app) {	//Lo tendria que tener?
-//		System.out.print("!---------------------------------!"
-//				 + "                                        "
-//				 + "Su estacionamiento fue dado de alta con exito."
-//				 + "Hora exacta: " + app.getHoraInicio()
-//				 + "Hora fin: " + "Pendiente"
-//				 + "Esta en Modo Manual, usted debe encargarse de finalizar el estacionamiento."
-//				 + "De lo contrario, la hora de fin quedara establecida hasta que:"
-//				 + "Sean las 20:00."
-//				 + "Usted mismo finalize el estacionamiento antes de las 20:00,"
-//				 + "descontando de su saldo el credito disponible segun la cantidad de horas que estuvo"
-//				 + "o de lo contrario descontando saldo de mas si no alcanza a cubrir el equivalente de"
-//				 + "saldo acreditado con la cantidad de horas que dejo su coche"
-//				 + "Notese que le quedara saldo negativo para esta ultima opcion"
-//				 + "!---------------------------------!");
-//	}
-
-//	@Override								
-//	public void darRespuestaFinal(AplicacionSEM app) {       //Lo tendria que tener?
-//		System.out.print("!---------------------------------!"
-//				 + "Su estacionamiento fue dado de baja con exito."
-//				 + "Hora exacta: " + app.getHoraInicio() + ":" + app.minutoInicio()
-//				 + "Hora fin: " + app.calcularHoraFin() + ":" + app.minutoFin()
-//				 + "Duracion " + (app.horaFinal() - app.getHoraInicio())
-//				 + "Costo: " + app.calcularCreditoAPagar()
-//				 + "!---------------------------------!");
-//	}
 	@Override
-	public void avisoDeCambio() {
-		System.out.print("!---------------------------------!"
-				+ "Usted a elegido el Modo Manual");
+	public void avisoDeCambio() {//Se muestra en la app, no se envia al usuario
+		System.out.print( "Usted a elegido el Modo Manual");
 	}
+	
 	@Override
 	public void inicioDeEstacionamiento(AplicacionSEM app) {
-		System.out.print("Alerta: No se ha iniciado el estacionamiento por que usted elijio el modo Manual"
-				+ "Procure iniciar el estacionamiento para evitar una infraccion");
+		
 	}
 
 	@Override
 	public void finDeEstacionamiento(AplicacionSEM app) {
-		System.out.print("Alerta: No se ha finalizado el estacionamiento por que usted elijio el modo Manual"
-				+ "Procure finalizar el estacionamiento para evitar descontar saldo de mas");
+		
 	}
 
+	@Override
+	public void puedeAlertarInicio(AplicacionSEM app) throws ExcepcionPersonalizada {
+		
+	}
 
-	
-	
+	@Override
+	public void puedeAlertarFin(AplicacionSEM app) throws ExcepcionPersonalizada {
+		
+	}
 }
